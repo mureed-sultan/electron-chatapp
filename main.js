990a8ca93b2a8d43266b54c008646e75e3bdf73b
Mureed Sultan <mureedsultangeni@gmail.com>
2023-07-27 02:11:29 +0500

    chat section pending

--- main.js
+++ main.js
@@ -6,106 +6,80 @@ const firebaseConfig = {
   storageBucket: "electron-59067.appspot.com",
   messagingSenderId: "910549463067",
   appId: "1:910549463067:web:da2a4ad2e3e4c9639c08be",
-  measurementId: "G-9SF86WJ68J"
+  measurementId: "G-9SF86WJ68J",
 };
 
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { initializeApp } = require('firebase/app');
-const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } = require('firebase/auth');
-require('firebase/auth');
-require('firebase/firestore');
-require('firebase/database'); 
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { initializeApp } = require("firebase/app");
+const {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} = require("firebase/auth");
+require("firebase/auth");
+require("firebase/firestore");
 
+// require('./script/firebase.js')
 const appFirebase = initializeApp(firebaseConfig);
 const auth = getAuth(appFirebase);
 
 
-
-
-function showUserList() {
-  const userList = document.getElementById('userList');
-  userList.innerHTML = ''; // Clear the existing user list
-
-  // Get a reference to the 'users' node in the database
-  const usersRef = ref(database, 'users');
-
-  // Attach a listener to fetch user data when it changes
-  onValue(usersRef, (snapshot) => {
-    snapshot.forEach((childSnapshot) => {
-      const user = childSnapshot.val();
-      const listItem = document.createElement('li');
-      listItem.innerText = user.firstName;
-      userList.appendChild(listItem);
-    });
-  });
-}
-// createChatRoom()
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      preload: __dirname + '/preload.js', 
+      preload: __dirname + "/preload.js",
     },
   });
 
-  mainWindow.loadFile('./pages/chat.html');
-  win.webContents.on('did-finish-load', () => {
-    showUserList();
-  });
+  mainWindow.loadFile("./pages/chat.html");
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
 
 function loginWithEmailAndPassword(email, password) {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
       mainWindow.loadFile(`./pages/chat.html`);
-      console.log('User logged in:', user.email);
-
+      console.log("User logged in:", user.email);
     })
     .catch((error) => {
-      console.error('Login error:', error);
+      console.error("Login error:", error);
     });
 }
 
-const userRegister = (userEmail, userPass)=>{
+const userRegister = (userEmail, userPass) => {
   createUserWithEmailAndPassword(auth, userEmail, userPass)
-  .then((userCredential) => {    
-    console.log('User registration successful:', userCredential.user.email);
-    mainWindow.loadFile(`./pages/chat.html`);
-  })
-  .catch((error) => {
-    console.error('User registration error:', error.code);
-  });
-}
-
-
-
-
-
-ipcMain.on('login', (event, email, password) => {
+    .then((userCredential) => {
+      console.log("User registration successful:", userCredential.user.email);
+      mainWindow.loadFile(`./pages/chat.html`);
+    })
+    .catch((error) => {
+      console.error("User registration error:", error.code);
+    });
+};
+ipcMain.on("login", (event, email, password) => {
   loginWithEmailAndPassword(email, password);
-  event.reply('login-response', 'Login request received.');
+  event.reply("login-response", "Login request received.");
 });
 
-ipcMain.on('save-user-data', (event, userObj) => {
-  userRegister(userObj.firstName+'@email.com', userObj.lastName)
+ipcMain.on("save-user-data", (event, userObj) => {
+  userRegister(userObj.firstName + "@email.com", userObj.lastName);
 });
